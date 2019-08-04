554a1aa56f5de76002c8d925fc2d8295ed43b43e
feiyinliao <feiyinliao@gmail.com>
2019-08-04 20:30:38 +0800

    add: 文件及富文本上传

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -1,19 +1,27 @@
 package com.mmall.controller.backend;
 
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.Product;
 import com.mmall.pojo.User;
+import com.mmall.service.IFileService;
 import com.mmall.service.IProductService;
 import com.mmall.service.IUserService;
+import com.mmall.util.PropertiesUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/manage/product")
@@ -21,9 +29,10 @@ public class ProductManageController {
 
   @Autowired
   IUserService iUserService;
-
   @Autowired
   IProductService iProductService;
+  @Autowired
+  IFileService iFileService;
 
   @RequestMapping(value = "/save", method = RequestMethod.POST)
   public ServerResponse productSave(HttpSession session, Product product) {
@@ -95,6 +104,68 @@ public class ProductManageController {
       return ServerResponse.createByErrorMessage("用户无权限");
     }
     return iProductService.searchProductList(productName, productId, pageNum, pageSize);
+  }
+
+  // 文件上传
+  @RequestMapping(value = "/upload")
+  public ServerResponse upload(HttpSession session,
+                               @RequestParam(value = "upload_file") MultipartFile file,
+                               HttpServletRequest request) {
+    User user = (User)session.getAttribute(Const.CURRENT_USER);
+    if (user == null) {
+      return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
+    }
+
+    if (!iUserService.checkAdminRole(user).isSuccess()) {
+      return ServerResponse.createByErrorMessage("用户无权限");
+    }
+
+    String path = request.getSession().getServletContext().getRealPath("upload");
+    String targetFileName = iFileService.upload(file, path);
+    String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+
+    Map<String, String> map = Maps.newHashMap();
+    map.put("uri", targetFileName);
+    map.put("url", url);
+    return ServerResponse.createBySuccess(map);
+  }
+
+  // 富文本中图片上传
+  @RequestMapping(value = "/richTextImgUpload")
+  public Map richTextImgUpload(HttpSession session,
+                               @RequestParam(value = "upload_file") MultipartFile file,
+                               HttpServletResponse response,
+                               HttpServletRequest request) {
+
+    Map resMap = Maps.newHashMap();
+    User user = (User)session.getAttribute(Const.CURRENT_USER);
+    if (user == null) {
+      resMap.put("success", false);
+      resMap.put("msg", "请登录管理员");
+      return resMap;
+    }
+
+    if (!iUserService.checkAdminRole(user).isSuccess()) {
+      resMap.put("success", false);
+      resMap.put("msg", "用户无权限");
+      return resMap;
+    }
+
+    // 富文本编辑器使用 simditor
+    // 所以返回的响应需要符合 simditor 的要求：{"success"; true, "msg": "", "file_path": []}
+    String path = request.getSession().getServletContext().getRealPath("upload");
+    String targetFileName = iFileService.upload(file, path);
+    if (StringUtils.isBlank(targetFileName)) {
+      resMap.put("success", false);
+      resMap.put("msg", "上传文件失败");
+      return resMap;
+    }
+
+    response.addHeader("Access-Allow-Control-Headers", "X-File-Name");
+    String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+    resMap.put("success", true);
+    resMap.put("file_path", url);
+    return resMap;
   }
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+
+springmvc 上传文件
+<form name="form1" action="/manage/product/upload" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file" />
+    <input type="submit" value="上传文件" />
+</form>
 </body>
 </html>
